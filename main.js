f405e5da9d3d9f07268f96704e3ed53b5454b4d2
sophmoreiraa <129889744+sophmoreiraa@users.noreply.github.com>
2023-04-05 12:06:56 -0300

    mudanças

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ document.addEventListener ("click" , showcolor );
 
 document.getElementById ( "botao" ).addEventListener ( "click" , shownome );
 
-function showcolor() 
+document.getElementById ( "btn-soma" ).addEventListener ( "click" , soma );
+
+
+function showcolor () 
 {
     document.body.style.backgroundColor = "red";
 }
@@ -12,4 +15,11 @@ function shownome ()
     let usuario = document.getElementById("nome").value;
     document.getElementById("mensagem").innerHTML = "Meu nome é:" + usuario;
     document.getElementById("nome").value = "";
+}
+
+function soma ()
+{
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    document.getElementById("resultado1").innerHTML = parseFloat (num1 ) + parseFloat (num2);
 }
