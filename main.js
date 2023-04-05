7362dbbc45f43b18b52660dcb26d64d11c36cb9a
sophmoreiraa <129889744+sophmoreiraa@users.noreply.github.com>
2023-04-05 10:51:52 -0300

    mudanças

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 document.addEventListener ("click" , showcolor );
 
+document.getElementById ( "botao" ).addEventListener ( "click" , shownome );
+
 function showcolor() 
 {
     document.body.style.backgroundColor = "red";
