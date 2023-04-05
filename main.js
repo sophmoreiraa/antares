a3920f3f8be06f66d2c0457154a3d5e9da06e547
sophmoreiraa <129889744+sophmoreiraa@users.noreply.github.com>
2023-04-05 12:20:48 -0300

    mudanças

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ document.getElementById ( "botao" ).addEventListener ( "click" , shownome );
 
 document.getElementById ( "btn-soma" ).addEventListener ( "click" , soma );
 
+document.getElementById ( "btn-dividir" ).addEventListener ( "click" , dividir );
+
 
 function showcolor () 
 {
@@ -22,4 +24,11 @@ function soma ()
     let num1 = document.getElementById("num1").value;
     let num2 = document.getElementById("num2").value;
     document.getElementById("resultado1").innerHTML = parseFloat (num1 ) + parseFloat (num2);
+}
+
+function dividir ()
+{
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    document.getElementById("resultado21").innerHTML = parseFloat (num1 ) / parseFloat (num2);
 }
